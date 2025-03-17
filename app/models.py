a1d77af432389cad78d7c0428a2d0e12a5bda453
shuntagami <shuntagami23@gmail.com>
2025-03-17 18:45:48 +0900

    update models and run make migrate-create

--- app/models.py
+++ app/models.py
@@ -13,13 +13,11 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    identifier = Column(String, unique=True, nullable=False)
     createdAt = Column(String, default=lambda: datetime.utcnow().isoformat())
     updatedAt = Column(String, default=lambda: datetime.utcnow().isoformat())
 
     def __repr__(self):
         return "<User('identifier={}', metadata={}, createdAt={})>".format(
-            self.identifier,
             self.createdAt,
             self.updatedAt,
         )
