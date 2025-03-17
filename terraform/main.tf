af402cf304af21e37cf34c3287a747b3740eac85
shuntagami <shuntagami23@gmail.com>
2025-03-17 16:55:55 +0900

    health check

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,7 +50,7 @@ resource "azurerm_linux_web_app" "this" {
     always_on = true
 
     # ヘルスチェックの設定
-    health_check_path                = "/api/health"
+    health_check_path                 = "/api/health"
     health_check_eviction_time_in_min = 2
   }
 
