0b8fbd812964571510cdbeb85e7d0258714a5a06
shuntagami <shuntagami23@gmail.com>
2025-04-15 10:42:45 +0900

    remove

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,10 +48,6 @@ resource "azurerm_linux_web_app" "this" {
     }
 
     always_on = true
-
-    # ヘルスチェックの設定
-    health_check_path                 = "/api/health"
-    health_check_eviction_time_in_min = 2
   }
 
   # すでにマネージドIDを使うのであれば、DOCKER_REGISTRY_SERVER_USERNAMEやPASSWORDは不要。
